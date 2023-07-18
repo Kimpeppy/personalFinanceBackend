cb1198ed2981cc37a0b9e8ae07362e440c88e300
Michael Kim <mike02kim@gmail.com>
2023-07-18 03:35:22 -0400

    Set up template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Register from './pages/Register'
 import Header from './components/Header'
+
+
 
 function App() {
   return (
@@ -17,6 +21,7 @@ function App() {
           </Routes>
         </div>
       </Router>
+      <ToastContainer/>
     </>
     
   );
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
+  
+
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+    </>
   )
 }
 
